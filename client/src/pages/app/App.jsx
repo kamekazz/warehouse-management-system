c0edd43e687fc1665f2b74183935c32b1d6cd936
kamekazz <taveras78@gmail.com>
2019-08-24 18:16:00 -0400

    fix 404 page on app route

--- client/src/pages/app/App.jsx
+++ client/src/pages/app/App.jsx
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import LayoutCop from '../../Layout/LayoutCop';
 import LocationPage from '../app/LocationPage';
 import ProductPage from '../app/ProductPage';
 import SecretorPage from '../app/SecretoPage';
 import LocationCreatePage from './LocationCreate/LocationCreatePage';
+import Error404Page from '../Error404Page';
 
 export class AppMainRoute extends Component {
   render() {
     const { match } = this.props;
 
     return (
-      <LayoutCop>
-        <Route exact path={`${match.path}`} component={SecretorPage} />
-        <Route exact path={`${match.path}/location`} component={LocationPage} />
-        <Route
-          exact
-          path={`${match.path}/location/create`}
-          component={LocationCreatePage}
-        />
-        <Route exact path={`${match.path}/product`} component={ProductPage} />
-      </LayoutCop>
+      <Switch>
+        <LayoutCop>
+          <Route exact path={`${match.path}`} component={SecretorPage} />
+          <Route
+            exact
+            path={`${match.path}/location`}
+            component={LocationPage}
+          />
+          <Route
+            exact
+            path={`${match.path}/location/create`}
+            component={LocationCreatePage}
+          />
+          <Route exact path={`${match.path}/product`} component={ProductPage} />
+          <Route component={Error404Page} />
+        </LayoutCop>
+      </Switch>
     );
   }
 }
