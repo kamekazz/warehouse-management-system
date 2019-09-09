4f75c1b766d239a838fd4b7411900b1a6fcb2bd6
kamekazz <taveras78@gmail.com>
2019-09-08 21:34:58 -0400

    stated pagination

--- client/src/redux/Location/location.reducer.js
+++ client/src/redux/Location/location.reducer.js
@@ -1,13 +1,24 @@
-import { LOCATION_QUERY } from '../types';
+import {
+  LOCATION_QUERY,
+  PAGINATION_FOR_LOCATION,
+  LAST_QUERY_BODY
+} from '../types';
 
 const initialState = {
-  queryData: []
+  queryData: [],
+  pagination: 50,
+  page: 4,
+  lastQuery: null
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case LOCATION_QUERY:
       return { ...state, queryData: payload };
+    case PAGINATION_FOR_LOCATION:
+      return { ...state, pagination: payload.pagination, page: payload.page };
+    case LAST_QUERY_BODY:
+      return { ...state, lastQuery: payload };
     default:
       return state;
   }
